19b821cac669e566b2c4ee6a5b9fb33cf2986d42
Lucas Matheus <ffmatheus6@GMAIL.COM>
2025-08-15 09:59:09 -0300

    up

--- src/assets/components/contact/Contact.jsx
+++ src/assets/components/contact/Contact.jsx
@@ -5,7 +5,7 @@ function Contact() {
     return (
         <div className='container-contact'>
             <a
-                href="https://wa.me/5511999999999"
+                href="https://wa.me/5561985787089"
                 target="_blank"
                 rel="noopener noreferrer"
             >
--- src/assets/components/presentation/Social.jsx
+++ src/assets/components/presentation/Social.jsx
@@ -6,13 +6,13 @@ import { FaInstagram, FaWhatsapp, FaMapMarkerAlt } from 'react-icons/fa';
 function Social() {
     return (
         <div className="social-container">
-            <a href="https://instagram.com/seuPerfil" target="_blank" rel="noopener noreferrer">
+            <a href="https://www.instagram.com/rebeca_desing_de_sobrancelhas" target="_blank" rel="noopener noreferrer">
                 <FaInstagram className="social-icon"/>
             </a>
-            <a href="https://wa.me/5511999999999" target="_blank" rel="noopener noreferrer">
+            <a href="https://wa.me/5561985787089" target="_blank" rel="noopener noreferrer">
                 <FaWhatsapp className="social-icon" />
             </a>
-            <a href="https://www.google.com/maps/place/Sua+Localização" target="_blank" rel="noopener noreferrer">
+            <a href="https://maps.app.goo.gl/MWuNBbLtKFWhm1PL7" target="_blank" rel="noopener noreferrer">
                 <FaMapMarkerAlt className="social-icon" />
             </a>
         </div>
